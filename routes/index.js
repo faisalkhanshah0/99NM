b9dbb5158075e3dea0110266faa23a01f64461b3
faisal <faisal@faisal-ubuntu.igglobal.com>
2018-09-11 16:43:21 +0530

    settings updated

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // fs.readFile('test.json', function(err, result){
+  //     let parsedjson = JSON.parse(result);
+  //     res.json(parsedjson);
+  // });
   res.render('index', { title: 'Express' });
 });
 
