54289a7fbbc1df214985f819a137367b780c398b
shah <shah@example.com>
2018-09-17 23:54:59 +0530

    version 1.0

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ var logger = require('morgan');
 const helmet = require('helmet')
 var compression = require('compression')
 
+
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var apisRouter = require('./routes/api');
 
 var app = express();
 
@@ -15,6 +16,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+require('./hbsregistry.js');
+
 
 app.use(helmet());
 app.use(compression());
@@ -25,7 +29,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apisRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,46 @@ var router = express.Router();
 var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // fs.readFile('test.json', function(err, result){
-  //     let parsedjson = JSON.parse(result);
-  //     res.json(parsedjson);
-  // });
   res.render('index', { title: 'Express' });
 });
-
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+router.get('/privacy-policy', function(req, res, next) {
+  res.render('privacy-policy', { title: 'Express' });
+});
+router.get('/refund-policy', function(req, res, next) {
+  res.render('refund-policy', { title: 'Express' });
+});
+router.get('/terms-of-use', function(req, res, next) {
+  res.render('termsofuse', { title: 'Express' });
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+router.get('/disclaimer', function(req, res, next) {
+  res.render('disclaimer', { title: 'Express' });
+});
+router.get('/post-an-ad', function(req, res, next) {
+  res.render('post-an-ad', { title: 'Express' });
+});
+router.get('/business-categories', function(req, res, next) {
+  res.render('categories', { title: 'Express' });
+});
+router.get('/articles', function(req, res, next) {
+  res.render('articles-listing', { title: 'Express' });
+});
+router.get('/:keyword', function(req, res, next) {
+  res.render('business-listing', { title: 'Express' });
+});
+router.get('/article/:title', function(req, res, next) {
+  res.render('article', { title: 'Express' });
+});
+router.get('/:city/:area/:title/:id', function(req, res, next) {
+  console.log(req.params);
+  res.render('detail-page', { title: 'Express' });
+});
+router.get('*', function(req, res, next) {
+  res.render('404', { title: '404 Page not Found !' });
+});
 module.exports = router;
