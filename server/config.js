703b1f7a80a7b6d0ed32793d1a88e5768b1c84fa
root <root@ip-172-31-26-238.ap-south-1.compute.internal>
2018-09-23 21:51:29 +0000

    little fix from remote

--- server/config.js
+++ server/config.js
@@ -1,9 +1,13 @@
 try {
     if(!process.env.APP_ENVIRONMENT || process.env.APP_ENVIRONMENT === 'local') {
-        require('dotenv').config({path: './config/local.conf'});
+    console.log('testing from config its local');
+    require('dotenv').config({path: './config/local.conf'});
     } else if(process.env.APP_ENVIRONMENT === 'prod') {
-        require('dotenv').config({path: './config/prod.conf'});       
+        console.log('testing from config its prod');
+        require('dotenv').config({path: '.././config/prod.conf'});       
     } else {
+
+        console.log('testing from config its else');
         throw new Error('config file read error');
     }
   } catch(e) {
