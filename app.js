c7b786087356d48a03091baabb1fd16853e27c1a
root <root@example.com>
2018-10-27 14:33:58 +0530

    error test

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  console.log('faizal error handler one');
   next(createError(404));
 });
 
@@ -62,7 +63,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  console.log('faizal error handler two');
   // render the error page
   res.status(err.status || 500);
   res.render('error');
